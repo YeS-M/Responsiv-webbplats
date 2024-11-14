379686b025bbc599256c9dfa9f8a6cfc921b55a8
YeS-M <sebastianssb100@gmail.com>
2024-11-14 12:43:26 +0100

    Update nyhetsbrev.php

--- nyhetsbrev.php
+++ nyhetsbrev.php
@@ -1,9 +1,12 @@
 <html>
-    <body>
+<body>
         
-<form action="nyhetsbrev.php" method="POST">
 Hej <?php echo $_POST["förnamn"]; ?>!<br><br>
 Tack för din anmälan till vårt nyhetsbrev!<br><br>
 Din e-postadress är: <?php echo $_POST["email"]; ?>
+
 </body>
 </html>
+
+
+
